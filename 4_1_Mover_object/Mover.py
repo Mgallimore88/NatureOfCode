9ed9b756ebd26584dd402984c8b913a220307e88
mgallimore88 <mgallimore88@gmail.com>
2019-06-12 14:37:43 +0100

    Adding new project 4.1 mover object. single mover object which can be blown by wind whose strength and direction depends on the mouse position.

---
+++ 4_1_Mover_object/Mover.py
@@ -0,0 +1,49 @@
+from p5 import *
+    
+
+class Mover:
+    def __init__(self, width, height, start_x, start_y):
+        self.location = Vector(start_x, start_y)
+        self.velocity = Vector(0,0)
+        self.mass = 1.5
+        self.acceleration = Vector(0.01, 0.01)
+    
+    def applyForce(self, force):
+        self.acceleration += force /self.mass
+
+    def update(self):
+        self.location += self.velocity
+        self.velocity += self.acceleration
+        self.acceleration *=0
+
+        
+    def display(self):
+        stroke(250)
+        fill(127)
+        circle((self.location.x,self.location.y),self.mass*20)
+
+    def follow(self, mouse_x, mouse_y):
+        self.location.x = mouse_x
+        self.location.y = mouse_y
+        self.velocity *= 0
+        self.acceleration *= 0
+        
+    def keep_inside_window(self):
+        if 0>= self.location.x: 
+            self.location.x = 1
+            self.velocity.x *= -1
+
+        elif self.location.x >= width:
+            self.location.x = width - 1
+            self.velocity.x *= -1
+
+        elif 0>= self.location.y:
+            self.location.y = 1
+            self.velocity.y *= -1
+
+        elif self.location.y >= height:
+            self.location. y = height-1
+            self.velocity.y *= -1
+
+
+    
---
+++ 4_1_Mover_object/main.py
@@ -0,0 +1,29 @@
+from p5 import *
+from Mover import *
+from wind import *
+
+def setup():
+    
+    global movers
+    size(640,480)
+    movers = Mover(width,height, 100, 100)
+
+def draw():
+    global movers
+    background(0)
+
+    movers.update()
+    movers.display()
+
+    movers.keep_inside_window()
+
+    if mouse_is_pressed:
+        movers.follow(mouse_x, mouse_y)
+    
+    if key_is_pressed:
+        wind = Wind(mouse_x, mouse_y)
+        wind_force = wind.wind_force(movers.location)
+        movers.applyForce(wind_force)
+    
+
+run()

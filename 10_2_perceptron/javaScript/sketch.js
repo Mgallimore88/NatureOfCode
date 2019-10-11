cb14f0b52319c07f8b2c2491b28d8145c4e2adde
mgallimore88 <mgallimore88@gmail.com>
2019-10-11 14:36:50 +0100

    adding gif to 4_8 particle system

---
+++ 10_2_perceptron/javaScript/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+  background(0);
+}
+
+function draw() {
+  // put drawing code here
+
+  fill(20);
+  background(100);
+
+  ellipse(50, 50);
+
+}
